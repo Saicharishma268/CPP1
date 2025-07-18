a53668c56cfdfd441af8c5b47295f8b2a103041d
Sai Charishma <saicharishmamannuru1@gmail.com>
2025-07-18 07:27:47 +0530

    linkedlist.h

---
+++ linkedlist.h
@@ -0,0 +1,47 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+struct node
+{
+	int data;
+	node*next;
+	node(int val)
+	{
+		data=val;
+		next=nullptr;
+	}
+};
+struct linkedlist{
+	node*head=nullptr;
+	node*temp=nullptr;
+	
+	void insert(int val)
+	{
+		node*newnode=new node(val);
+		if(head=nullptr)
+		{
+			head=newnode;
+			temp=newnode;
+		}
+		else
+		{
+			temp->next=newnode;
+			temp=newnode;
+		}
+		
+	}
+	void display()
+	{
+		node*current=head;
+		while(current!=nullptr)
+		{
+			cout<<current->data<<" ";
+			current=current->next;
+		}
+	}
+		 
+};
